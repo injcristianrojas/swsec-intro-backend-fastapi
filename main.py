0e712070a1682614081299c4dbebe981005762e0
Cristián Rojas <injcristianrojas@gmail.com>
2024-09-03 17:17:25 -0400

    Messages work OK

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+from db import init_db
 
 from v1.endpoints import router as v1_router
+
+init_db()
 
 app = FastAPI()
 
--- v1/endpoints.py
+++ v1/endpoints.py
@@ -1,7 +1,31 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
+from db import connect_db
 
 router = APIRouter()
 
-@router.get("/items")
-def get_items():
-    return {"version": "v1", "items": ["item1", "item2"]}
+class Message(BaseModel):
+    message: str
+
+@router.get("/messages")
+async def get_messages():
+    conn = connect_db()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM messages")
+    results = cur.fetchall()
+    conn.close()
+    json_results = []
+    for row in results:
+        json_results.append({"id": row[0], "message": row[1]})
+    return json_results
+
+@router.post("/messages/add")
+async def post_message(message: Message):
+    message = message.message
+    print(message)
+    conn = connect_db()
+    cur = conn.cursor()
+    cur.execute("INSERT INTO messages(message) VALUES ('" + message + "')")
+    conn.commit()
+    conn.close()
+    return [{"status": "OK"}]
