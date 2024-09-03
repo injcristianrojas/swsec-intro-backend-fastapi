2fce2fde5653fe28341a2414868ca41c6b38873d
Cristián Rojas <injcristianrojas@gmail.com>
2024-09-03 19:30:58 -0400

    Shell injection

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import subprocess
 from fastapi import FastAPI
 from db import init_db
 
@@ -10,6 +11,12 @@ app = FastAPI()
 
 app.include_router(v1_router, prefix="/api/v1")
 app.include_router(v2_router, prefix="/api/v2")
+
+@app.get("/healthcheck")
+@app.get("/healthcheck/{file}")
+async def healthcheck(file = "healthckeck"):
+    data = subprocess.check_output('cat %s' % file, shell=True, text=True)
+    return [{"status": data}]
 
 @app.get("/")
 async def root():
