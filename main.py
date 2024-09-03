fe74ddbf49f0a40bad45b21f4f48f5ab3a134adb
Cristián Rojas <injcristianrojas@gmail.com>
2024-09-03 19:40:32 -0400

    OpenAPI

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import subprocess
 from fastapi import FastAPI
+from fastapi.openapi.utils import get_openapi
 from db import init_db
 
 from v1.endpoints import router as v1_router
@@ -9,12 +10,14 @@ init_db()
 
 app = FastAPI()
 
-app.include_router(v1_router, prefix="/api/v1")
+app.include_router(v1_router, prefix="/api/v1", include_in_schema=False)
 app.include_router(v2_router, prefix="/api/v2")
+
+print(app.routes)
 
 @app.get("/healthcheck")
 @app.get("/healthcheck/{file}")
-async def healthcheck(file = "healthckeck"):
+async def healthcheck(file = "healthcheck"):
     data = subprocess.check_output('cat %s' % file, shell=True, text=True)
     return [{"status": data}]
 
