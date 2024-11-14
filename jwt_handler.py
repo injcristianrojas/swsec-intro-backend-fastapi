be4a8eb98e470e2ffe1f49d7461f9a8996dbe806
Cristián Rojas <injcristianrojas@gmail.com>
2024-11-13 22:26:06 -0300

    Another function fix

--- jwt_handler.py
+++ jwt_handler.py
@@ -30,7 +30,7 @@ def verify_token(token, credentials_exception):
         raise credentials_exception
 
 
-def get_current_user(token: str = Depends(oauth2_scheme)):
+def get_current_user(token=Depends(oauth2_scheme)):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
