bed02ac87f17a021c66ff1116ef80f0b93b18f6a
Cristián Rojas <injcristianrojas@gmail.com>
2025-09-03 22:34:53 -0400

    V2 Ready

--- app/v2/endpoints.py
+++ app/v2/endpoints.py
@@ -3,7 +3,7 @@ from typing import Annotated
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlmodel import Session, select
 
-from ..database import User, UserLogin, Token, engine
+from ..database import Message, MessageInsert, User, UserLogin, Token, engine
 from ..jwt_handler import create_access_token, get_current_user
 
 SECRET_KEY = "123"
@@ -30,39 +30,28 @@ async def login_for_access_token(user_login: UserLogin):
         return Token(token=token)
 
 
-# @router.get("/messages")
-# async def get_messages(current_user: Annotated[User, Depends(get_current_user)]):
-#     conn = connect_db()
-#     cur = conn.cursor()
-#     cur.execute("SELECT * FROM messages")
-#     results = cur.fetchall()
-#     conn.close()
-#     json_results = []
-#     for row in results:
-#         json_results.append(Message(id=row[0], message=row[1]))
-#     return json_results
+@router.get("/messages", response_model=list[Message])
+async def get_messages(current_user: Annotated[User, Depends(get_current_user)]):
+    with Session(engine) as session:
+        messages = session.exec(select(Message)).all()
+        return messages
 
 
-# @router.post("/messages/add")
-# async def post_message(message: MessageInput, current_user: Annotated[User, Depends(get_current_user)]):
-#     message = message.message
-#     print(message)
-#     conn = connect_db()
-#     cur = conn.cursor()
-#     cur.execute("INSERT INTO messages(message) VALUES ('" + message + "')")
-#     conn.commit()
-#     conn.close()
-#     return [{"status": "OK"}]
+@router.post("/messages/add")
+async def post_message(
+    message_insert: MessageInsert,
+    current_user: Annotated[User, Depends(get_current_user)],
+):
+    with Session(engine) as session:
+        session.add(Message(message=message_insert.message))
+        session.commit()
+    return [{"status": "OK"}]
 
 
-# @router.get("/users/type/{id}")
-# async def get_users_by_type(id, current_user: Annotated[User, Depends(get_current_user)]):
-#     conn = connect_db()
-#     cur = conn.cursor()
-#     cur.execute("SELECT * FROM users WHERE user_type = " + id)
-#     results = cur.fetchall()
-#     conn.close()
-#     json_results = []
-#     for row in results:
-#         json_results.append({"id": row[0], "username": row[1]})
-#     return json_results
+@router.get("/users/type/{id}", response_model=list[User])
+async def get_users_by_type(
+    id, current_user: Annotated[User, Depends(get_current_user)]
+):
+    with Session(engine) as session:
+        user = session.exec(select(User).where(User.user_type == id)).all()
+        return user
